5fc69193656075d034ae44c05f1c568165c39ebf
Michael Bowman <bowmanmc@gmail.com>
2017-07-19 10:48:32 -0400

    update coverage configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 function tle2czml() {
-
+    console.log('tle2czml()');
 }
 
 export default tle2czml;
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,6 +2,6 @@ import tle2czml from './index';
 
 describe('tle2czml()', () => {
     it('converts single tle', () => {
-        console.log('running tle2czml test...');
+        tle2czml();
     });
 });
