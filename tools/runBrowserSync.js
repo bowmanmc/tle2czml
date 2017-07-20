c5c7cc8dcb3522b913121c1f4043de74ac7bd799
Michael Bowman <bowmanmc@gmail.com>
2017-07-20 11:09:13 -0400

    add viewer to test with

---
+++ tools/runBrowserSync.js
@@ -0,0 +1,22 @@
+//import browserSync from 'browser-sync';
+let browserSync = require('browser-sync');
+
+
+browserSync({
+    'files': [
+        'www/**/*'
+    ],
+    'watchOptions': {
+        'ignoreInitial': true
+    },
+    'server': {
+        'baseDir': 'www',
+    },
+    'port': 9000,
+    'middleware': [],
+    'reloadOnRestart': false,
+    'notify': false,
+    'open': false
+    //'open': 'local',
+    //'browser': 'chrome',
+});
